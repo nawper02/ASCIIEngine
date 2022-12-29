e969391517c4010b616968bd822e1cc144d7af7a
nawper02 <nawper02@gmail.com>
2022-12-29 14:38:47 -0700

    Started List View

--- ASCIIEngine_SRC/ListView.cs
+++ ASCIIEngine_SRC/ListView.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Class just like the 'Label' class, but interactable so it changes color when q is pressed
 
+using System;
 namespace ASCIIEngine_SRC
 {
-    internal class ListView
+    public class ListView : Interactive
     {
+        public List<string> strings;
+        public ListView(List<string> strings, int row, int col) : base(strings.Count, strings.Max(s => s.Length), row, col)
+        {
+            this.strings = strings;
+            this.AddAction(ConsoleKey.DownArrow, () => { CycleActiveDown(); });
+            this.AddAction(ConsoleKey.UpArrow, () => { CycleActiveUp(); });
+            Init();
+        }
+
+        public void Init()
+        {
+
+        }
+        public void CycleActiveUp()
+        {
+
+        }
+        public void CycleActiveDown()
+        {
+
+        }
     }
 }
